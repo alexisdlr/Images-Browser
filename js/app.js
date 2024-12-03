0fdf60599e4c17207ed33a2ac230dc8484cbdf07
Alexis De Leon <alxsdlrdev3009@gmail.com>
2024-12-02 20:01:25 -0600

    service-worker

--- js/app.js
+++ js/app.js
@@ -1,3 +1,72 @@
+if ('serviceWorker' in navigator) {
+  // Solicitar permiso de notificaciones
+  Notification.requestPermission().then(permission => {
+    if (permission === "granted") {
+      console.log("Permiso de notificación concedido.");
+
+      // Asegurarse de que el Service Worker esté listo
+      navigator.serviceWorker.register('/service_worker.js')
+        .then(registration => {
+          console.log('Service Worker registrado con éxito:', registration);
+
+          return navigator.serviceWorker.ready; // Espera hasta que el Service Worker esté activo
+        })
+        .then(async registration => {
+          console.log('Service Worker está listo para manejar notificaciones.');
+
+          // Verificar si ya existe una suscripción
+          const existingSubscription = await registration.pushManager.getSubscription();
+          if (!existingSubscription) {
+            console.log("No existe suscripción previa, creando una nueva...");
+
+            // Suscribirse a notificaciones push
+            const subscription = await registration.pushManager.subscribe({
+              userVisibleOnly: true,
+              applicationServerKey: urlBase64ToUint8Array('BLcKwqKaxq_4PBdpISd1O115WG7iMkfhq-BKEkh_jC2L67kPTgVYc_ZCQ5OTsggKcbffcY9g9j5Vn9yEasyhaO8') // Reemplaza con tu clave pública VAPID
+            });
+
+            console.log('Suscripción a Push:', JSON.stringify(subscription));
+
+            // Envía esta suscripción al servidor
+            await fetch('http://localhost:8000/subscribe', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(subscription)
+            });
+          } else {
+            console.log('Ya existe una suscripción activa:', existingSubscription);
+          }
+        })
+        .catch(error => {
+          console.error('Error al registrar el Service Worker o Push:', error);
+        });
+    } else {
+      console.warn("Permiso de notificación denegado.");
+    }
+  }).catch(error => {
+    console.error("Error al solicitar permiso de notificaciones:", error);
+  });
+} else {
+  console.warn('Service Workers o PushManager no son soportados por este navegador.');
+}
+
+// Convertir clave VAPID a formato Uint8Array
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+
 const resultado = document.querySelector('#resultado')
 const formulario = document.querySelector('#formulario')
 const paginacion = document.querySelector('#paginacion')
---
+++ service_worker.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = 'buscador-imagenes-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/custom.css', // Cambia por el nombre de tu archivo CSS
+  '/js/app.js',     // Cambia por el nombre de tu archivo JS
+  '/manifest.json',
+  '/icons/icon.png',
+];
+
+// Instalar el service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Archivos en caché');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activar el service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Eliminando caché antigua:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar solicitudes de red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener("push", (event) => {
+  if (event.data) {
+    try {
+      // Convierte los datos recibidos en un objeto JSON
+      const data = event.data.json();
+
+      console.log(data, 'notificacion')
+
+      // Muestra la notificación
+      event.waitUntil(
+        self.registration.showNotification(data.title || 'titulo', {
+          body: data.body || 'No hay',
+          icon: data.icon || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy_EXtATKPKHh9NQshUwkh-cm0WVBl5sI5dA&s', // Usa un icono predeterminado si no se proporciona
+        })
+        
+      );
+    } catch (error) {
+      console.error('Error procesando los datos de la notificación:', error);
+    }
+  } else {
+    console.warn('No se recibieron datos en el evento push.');
+  }
+});
+
+
+
+// Manejar clic en la notificación
+self.addEventListener('notificationclick', event => {
+  event.notification.close(); // Cierra la notificación
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      if (clientList.length > 0) {
+        // Si hay pestañas abiertas de la app, enfócalas
+        return clientList[0].focus();
+      }
+      // Si no hay pestañas abiertas, abre una nueva
+      return clients.openWindow('/');
+    })
+  );
+});
